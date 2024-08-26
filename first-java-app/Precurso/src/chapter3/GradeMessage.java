67610c6000c0e66ef8d40ca8f176bceb9096541c
Pablo Paradela <paradelapablo@gmail.com>
2024-08-26 18:00:47 +0200

    Switch done

---
+++ first-java-app/Precurso/src/chapter3/GradeMessage.java
@@ -0,0 +1,44 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class GradeMessage {
+
+        public static void main(String args[]){
+
+            //Get the test score
+            System.out.println("Enter your letter grade");
+            Scanner scanner = new Scanner(System.in);
+            String grade = scanner.next();
+
+            String message;
+
+            switch(grade){
+                case "A":
+                    message = "Excelent job";
+                    break;
+                case "B":
+                    message = "Good job";
+                    break;
+                case "C":
+                    message = "Ok job";
+                    break;
+                case "D":
+                    message = "Not so good job";
+                    break;
+                case "E":
+                    message = "Bad job";
+                    break;
+                default:
+                    message = "Error. Invalid grade";
+                    break;
+            }
+        System.out.println(message);
+
+    }
+
+
+
+
+
+}
